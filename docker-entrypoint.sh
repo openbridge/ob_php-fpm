662ac8ef9cd0bfec0e4f89c5b710cdd6ac590f71
tspicer <thomas@openbridge.com>
2018-10-30 00:03:33 -0400

    fix shell check errors, format

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#---------------------------------------------------------------------
+# configure php-fpm
+#---------------------------------------------------------------------
+
 function php-fpm() {
 
   # Determine the PHP-FPM runtime environment
@@ -25,78 +29,78 @@ function php-fpm() {
    if [[ -z $APP_DOCROOT ]]; then export APP_DOCROOT=/app && mkdir -p "${APP_DOCROOT}"; fi
 
   {
-              echo '[global]'
-              echo 'include=/etc/php7/php-fpm.d/*.conf'
+        echo '[global]'
+        echo 'include=/etc/php7/php-fpm.d/*.conf'
   } | tee /etc/php7/php-fpm.conf
 
   {
-              echo '[global]'
-              echo 'error_log = {{LOG_PREFIX}}/error.log'
-              echo
-              echo '[www]'
-              echo '; if we send this to /proc/self/fd/1, it never appears'
-              echo 'access.log = {{LOG_PREFIX}}/access.log'
-              echo
-              echo 'clear_env = no'
-              echo '; ping.path = /ping'
-              echo '; Ensure worker stdout and stderr are sent to the main error log.'
-              echo 'catch_workers_output = yes'
+        echo '[global]'
+        echo 'error_log = {{LOG_PREFIX}}/error.log'
+        echo
+        echo '[www]'
+        echo '; if we send this to /proc/self/fd/1, it never appears'
+        echo 'access.log = {{LOG_PREFIX}}/access.log'
+        echo
+        echo 'clear_env = no'
+        echo '; ping.path = /ping'
+        echo '; Ensure worker stdout and stderr are sent to the main error log.'
+        echo 'catch_workers_output = yes'
   } | tee /etc/php7/php-fpm.d/docker.conf
 
   {
-              echo '[global]'
-              echo 'daemonize = no'
-              echo 'log_level = error'
-              echo
-              echo '[www]'
-              echo 'user = www-data'
-              echo 'group = www-data'
-              echo 'listen = [::]:{{PHP_FPM_PORT}}'
-              echo 'listen.mode = 0666'
-              echo 'listen.owner = www-data'
-              echo 'listen.group = www-data'
-              echo 'pm = static'
-              echo 'pm.max_children = {{PHP_MAX_CHILDREN}}'
-              echo 'pm.max_requests = 1000'
-              echo 'pm.start_servers = {{PHP_START_SERVERS}}'
-              echo 'pm.min_spare_servers = {{PHP_MIN_SPARE_SERVERS}}'
-              echo 'pm.max_spare_servers = {{PHP_MAX_SPARE_SERVERS}}'
+        echo '[global]'
+        echo 'daemonize = no'
+        echo 'log_level = error'
+        echo
+        echo '[www]'
+        echo 'user = www-data'
+        echo 'group = www-data'
+        echo 'listen = [::]:{{PHP_FPM_PORT}}'
+        echo 'listen.mode = 0666'
+        echo 'listen.owner = www-data'
+        echo 'listen.group = www-data'
+        echo 'pm = static'
+        echo 'pm.max_children = {{PHP_MAX_CHILDREN}}'
+        echo 'pm.max_requests = 1000'
+        echo 'pm.start_servers = {{PHP_START_SERVERS}}'
+        echo 'pm.min_spare_servers = {{PHP_MIN_SPARE_SERVERS}}'
+        echo 'pm.max_spare_servers = {{PHP_MAX_SPARE_SERVERS}}'
   } | tee /etc/php7/php-fpm.d/zz-docker.conf
 
   {
-              echo 'max_executionn_time=300'
-              echo 'memory_limit={{PHP_MEMORY_LIMIT}}M'
-              echo 'error_reporting=1'
-              echo 'display_errors=0'
-              echo 'log_errors=1'
-              echo 'user_ini.filename='
-              echo 'realpath_cache_size=2M'
-              echo 'cgi.check_shebang_line=0'
-              echo 'date.timezone=UTC'
-              echo 'short_open_tag=Off'
-              echo 'session.auto_start=Off'
-              echo 'upload_max_filesize=50M'
-              echo 'post_max_size=50M'
-              echo 'file_uploads=On'
+        echo 'max_executionn_time=300'
+        echo 'memory_limit={{PHP_MEMORY_LIMIT}}M'
+        echo 'error_reporting=1'
+        echo 'display_errors=0'
+        echo 'log_errors=1'
+        echo 'user_ini.filename='
+        echo 'realpath_cache_size=2M'
+        echo 'cgi.check_shebang_line=0'
+        echo 'date.timezone=UTC'
+        echo 'short_open_tag=Off'
+        echo 'session.auto_start=Off'
+        echo 'upload_max_filesize=50M'
+        echo 'post_max_size=50M'
+        echo 'file_uploads=On'
 
-              echo
-              echo 'opcache.enable={{PHP_OPCACHE_ENABLE}}'
-              echo 'opcache.enable_cli=0'
-              echo 'opcache.save_comments=1'
-              echo 'opcache.interned_strings_buffer=8'
-              echo 'opcache.fast_shutdown=1'
-              echo 'opcache.validate_timestamps=2'
-              echo 'opcache.revalidate_freq=15'
-              echo 'opcache.use_cwd=1'
-              echo 'opcache.max_accelerated_files=100000'
-              echo 'opcache.max_wasted_percentage=5'
-              echo 'opcache.memory_consumption={{PHP_OPCACHE_MEMORY_CONSUMPTION}}M'
-              echo 'opcache.consistency_checks=0'
-              echo 'opcache.huge_code_pages=1'
-              echo
-              echo ';opcache.file_cache="{{CACHE_PREFIX}}/fastcgi/.opcache"'
-              echo ';opcache.file_cache_only=1'
-              echo ';opcache.file_cache_consistency_checks=1'
+        echo
+        echo 'opcache.enable={{PHP_OPCACHE_ENABLE}}'
+        echo 'opcache.enable_cli=0'
+        echo 'opcache.save_comments=1'
+        echo 'opcache.interned_strings_buffer=8'
+        echo 'opcache.fast_shutdown=1'
+        echo 'opcache.validate_timestamps=2'
+        echo 'opcache.revalidate_freq=15'
+        echo 'opcache.use_cwd=1'
+        echo 'opcache.max_accelerated_files=100000'
+        echo 'opcache.max_wasted_percentage=5'
+        echo 'opcache.memory_consumption={{PHP_OPCACHE_MEMORY_CONSUMPTION}}M'
+        echo 'opcache.consistency_checks=0'
+        echo 'opcache.huge_code_pages=1'
+        echo
+        echo ';opcache.file_cache="{{CACHE_PREFIX}}/fastcgi/.opcache"'
+        echo ';opcache.file_cache_only=1'
+        echo ';opcache.file_cache_consistency_checks=1'
   } | tee /etc/php7/conf.d/50-setting.ini
 
   mkdir -p "${CACHE_PREFIX}"/fastcgi/
@@ -113,19 +117,27 @@ function php-fpm() {
   find /etc/php7 -maxdepth 3 -type f -exec sed -i -e 's|{{PHP_MAX_CHILDREN}}|'"${PHP_MAX_CHILDREN}"'|g' {} \;
   find /etc/php7 -maxdepth 3 -type f -exec sed -i -e 's|{{LOG_PREFIX}}|'"${LOG_PREFIX}"'|g' {} \;
 
-
 }
+
+#---------------------------------------------------------------------
+# configure php connection to redis
+#---------------------------------------------------------------------
+
 function redis() {
 
 {
-              echo 'session.gc_maxlifetime=86400'
-              echo 'session.save_handler=redis'
-              echo 'session.save_path="tcp://{{REDIS_UPSTREAM}}?weight=1&timeout=2.5&database=3"'
+      echo 'session.gc_maxlifetime=86400'
+      echo 'session.save_handler=redis'
+      echo 'session.save_path="tcp://{{REDIS_UPSTREAM}}?weight=1&timeout=2.5&database=3"'
 } | tee /etc/php7/conf.d/zz-redis-setting.ini
 
   find /etc/php7 -maxdepth 3 -type f -exec sed -i -e 's|{{REDIS_UPSTREAM}}|'"${REDIS_UPSTREAM}"'|g' {} \;
 
 }
+
+#---------------------------------------------------------------------
+# configure the use of a plugin (install script) for wordpress or similiar
+#---------------------------------------------------------------------
 
 function install_plugin() {
 
@@ -139,22 +151,25 @@ fi
 
 }
 
+#---------------------------------------------------------------------
+# configure monit
+#---------------------------------------------------------------------
+
 function monit() {
 
-# Create monit config
 {
-              echo 'set daemon 10'
-              echo 'set pidfile /var/run/monit.pid'
-              echo 'set statefile /var/run/monit.state'
-              echo 'set httpd port 2849 and'
-              echo '    use address localhost'
-              echo '    allow localhost'
-              echo 'set logfile syslog'
-              echo 'set eventqueue'
-              echo '    basedir /var/run'
-              echo '    slots 100'
-              echo 'include /etc/monit.d/*'
-
+      echo 'set daemon 10'
+      echo '    with START DELAY 10'
+      echo 'set pidfile /var/run/monit.pid'
+      echo 'set statefile /var/run/monit.state'
+      echo 'set httpd port 2849 and'
+      echo '    use address localhost'
+      echo '    allow localhost'
+      echo 'set logfile syslog'
+      echo 'set eventqueue'
+      echo '    basedir /var/run'
+      echo '    slots 100'
+      echo 'include /etc/monit.d/*'
 } | tee /etc/monitrc
 
 # Start monit
@@ -167,19 +182,27 @@ function monit() {
 
 }
 
+#---------------------------------------------------------------------
+# make sure all permissions are set correctly for php-fpm
+#---------------------------------------------------------------------
+
 function permissions() {
 
     echo "Setting ownership and permissions on APP_ROOT and CACHE_PREFIX... "
 
     # This assumes you are using the common www-data for your user and group in NGINX and PHP-FPM. If you are using different users this is usually a recipe for error.
 
-    find ${APP_DOCROOT} ! -user www-data -exec /usr/bin/env bash -c "chown www-data:www-data {}" \;
-    find ${APP_DOCROOT} ! -perm 755 -type d -exec /usr/bin/env bash -c "chmod 755 {}" \;
-    find ${APP_DOCROOT} ! -perm 644 -type f -exec /usr/bin/env bash -c "chmod 644 {}" \;
-    find ${CACHE_PREFIX} ! -perm 755 -type d -exec /usr/bin/env bash -c "chmod 755 {}" \;
-    find ${CACHE_PREFIX} ! -perm 755 -type f -exec /usr/bin/env bash -c "chmod 755 {}" \;
+    find ${APP_DOCROOT} ! -user www-data -exec /usr/bin/env bash -c 'i="$1"; chown www-data:www-data "$i"' _ {} \;
+    find ${APP_DOCROOT} ! -perm 755 -type d -exec /usr/bin/env bash -c 'i="$1"; chmod 755  "$i"' _ {} \;
+    find ${APP_DOCROOT} ! -perm 644 -type f -exec /usr/bin/env bash -c 'i="$1"; chmod 644 "$i"' _ {} \;
+    find ${CACHE_PREFIX} ! -perm 755 -type d -exec /usr/bin/env bash -c 'i="$1"; chmod 755  "$i"' _ {} \;
+    find ${CACHE_PREFIX} ! -perm 644 -type f -exec /usr/bin/env bash -c 'i="$1"; chmod 644 "$i"' _ {} \;
 
 }
+
+#---------------------------------------------------------------------
+# run all the functions to start the services
+#---------------------------------------------------------------------
 
 function run() {
 
@@ -188,7 +211,6 @@ function run() {
   monit
   if [[ ! -z $NGINX_APP_PLUGIN ]]; then install_plugin else echo "OK: No plugins will be activated"; fi
   permissions
-
   echo "OK: All processes have completed. Service is ready..."
 }
 
